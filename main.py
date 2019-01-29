3f1e5b4561bf5091510549273248756f8cab1a86
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2019-01-29 14:30:12 +0100

    starting with Kivy tutorial

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+import better_exchook
+import argparse
+
+
+class KioskWidget(Widget):
+    pass
+
+
+class KioskApp(App):
+    def build(self):
+        return KioskWidget()
+
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    args = arg_parser.parse_args()
+
+    app = KioskApp()
+    app.run()
+
+
+if __name__ == '__main__':
+    better_exchook.install()
+    main()
