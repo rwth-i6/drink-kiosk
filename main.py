6a97b542410ad4a2da4c0f170b8b434f7452f456
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2019-01-29 15:24:06 +0100

    scrollview resize fix

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
-from kivy.app import runTouchApp
 
 
 class KioskWidget(Widget):
@@ -22,13 +21,14 @@ class KioskApp(App):
         layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
         # Make sure the height is such that there is something to scroll.
         layout.bind(minimum_height=layout.setter('height'))
-        for i in range(100):
+        for i in range(20):
             btn = Button(text=str(i), size_hint_y=None, height=40)
             layout.add_widget(btn)
+        # Window resize recursion error while using ScrollView? https://github.com/kivy/kivy/issues/5638
         root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
         root.add_widget(layout)
+        Window.bind(size=root.setter("size"))
         return root
-        #return KioskWidget()
 
 
 def main():
@@ -41,4 +41,5 @@ def main():
 
 if __name__ == '__main__':
     better_exchook.install()
+    kivy.require("1.10.0")
     main()
